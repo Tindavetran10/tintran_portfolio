e2e94de031cc2b4fa5811105e9d34a1ff79ff635
Tindavetran10 <102245594+Tindavetran10@users.noreply.github.com>
2025-04-30 03:03:07 +0700

    Finish a half of Projects section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,58 +1,53 @@
-import React, {useState} from 'react'
-import Globe from "react-globe.gl";
-import Button from "../components/Button.jsx";
+import { useState } from 'react';
+import Globe from 'react-globe.gl';
 
+import Button from '../components/Button.jsx';
 
 const About = () => {
-    const [hasCopied, setHasCopied] = useState(false)
-    
-    const handleCopy = () => {
-        navigator.clipboard.writeText('gade.tintran76@gmail.com');
+    const [hasCopied, setHasCopied] = useState(false);
 
-        setHasCopied(true)
+    const handleCopy = () => {
+        navigator.clipboard.writeText(' gade.tintran76@gmail.com');
+        setHasCopied(true);
+
         setTimeout(() => {
             setHasCopied(false);
         }, 2000);
     };
 
     return (
-        <section className={"c-space my-20"}>
-            <div className={"grid xl:grid-cols-3 xl: grid-rows-6 " +
-                "md:grid-cols-2 grid-cols-1 gap 5 h-full"}>
-                <div className={"col-span-1 xl:row-span-3"}>
-                    <div className={"grid-container"}>
-                        <img src="/assets/grid1.png" alt="grid-1"
-                             className="w-full sm:h-[276px] h-fit object-contain"/>
+        <section className="c-space my-20" id="about">
+            <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+                <div className="col-span-1 xl:row-span-3">
+                    <div className="grid-container">
+                        <img src="public/assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
 
                         <div>
-                            <p className={"grid-headtext"}>Hi, I'm Tin</p>
-
-                            <p className={"grid-subtext"}>
-                                Software engineer and game designer crafting immersive experiences.
-                                I balance technical expertise with creative design principles to build engaging games that resonate with players.
+                            <p className="grid-headtext">Hi, I’m Tin Tran</p>
+                            <p className="grid-subtext">
+                                I'm a game developer with a passion for creating immersive experiences. I specialize in Unity and C#, and I love
+                                exploring new technologies and techniques to push the boundaries of what's possible in game development.
                             </p>
                         </div>
                     </div>
                 </div>
 
-                <div className={"col-span-1 xl:row-span-3"}>
-                    <div className={"grid-container"}>
-                        <img src="/assets/grid2.png" alt="grid-2"
-                             className="w-full sm:w-[276px] h-fit object-contain"/>
+                <div className="col-span-1 xl:row-span-3">
+                    <div className="grid-container">
+                        <img src="public/assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain" />
 
                         <div>
-                            <p className={"grid-headtext"}>My Journey</p>
-
-                            <p className={"grid-subtext"}>
-                                I started my career as a software engineer, where I honed my skills in coding and problem-solving.
-                                My passion for gaming led me to explore game design, where I learned to create engaging narratives and mechanics.
+                            <p className="grid-headtext">Tech Stack</p>
+                            <p className="grid-subtext">
+                                I specialize in a variety of languages, frameworks, and tools that allow me to build robust and scalable
+                                applications
                             </p>
                         </div>
                     </div>
                 </div>
 
-                <div className={"col-span-1 xl:row-span-4"}>
-                    <div className={"grid-container"}>
+                <div className="col-span-1 xl:row-span-4">
+                    <div className="grid-container">
                         <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
                             <Globe
                                 height={326}
@@ -65,48 +60,48 @@ const About = () => {
                                 bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
                             />
                         </div>
-
                         <div>
                             <p className="grid-headtext">I’m very flexible with time zone communications & locations</p>
-                            <p className="grid-subtext">I'm based in Vietnam, but I am open to opportunities worldwide.</p>
-                            <Button name="Contact Me" isBeam containerClass="w-full mt-10"/>
+                            <p className="grid-subtext">I&apos;m based in Vietnam and open to remote work worldwide.</p>
+                            <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
                         </div>
                     </div>
                 </div>
 
                 <div className="xl:col-span-2 xl:row-span-3">
                     <div className="grid-container">
-                        <img src="/assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain"/>
+                        <img src="public/assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain" />
+
                         <div>
-                            <p className={"grid-headtext"}>My Passion for Gaming</p>
-                            <p className={"grid-subtext"}>
-                                I love playing video games as much as I enjoy creating them, giving me unique insight into what makes gameplay truly engaging.
-                                This dual perspective fuels my approach to game development, where player experience always comes first.
+                            <p className="grid-headtext">My Passion for Gaming</p>
+                            <p className="grid-subtext">
+                                I love playing games as much as making them. Game Developing isn&apos;t just my
+                                profession—it&apos;s my passion. I enjoy exploring new technologies, and enhancing my skills.
                             </p>
                         </div>
                     </div>
+                </div>
 
-                    <div className="xl:col-span-1 xl:row-span-2">
-                        <div className="grid-container">
-                            <img
-                                src="assets/grid4.png"
-                                alt="grid-4"
-                                className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
-                            />
+                <div className="xl:col-span-1 xl:row-span-2">
+                    <div className="grid-container">
+                        <img
+                            src="public/assets/grid4.png"
+                            alt="grid-4"
+                            className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
+                        />
 
-                            <div className="space-y-2">
-                                <p className="grid-subtext text-center">Contact me</p>
-                                <div className="copy-container" onClick={handleCopy}>
-                                    <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy" />
-                                    <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">gade.tintran76@gmail.com</p>
-                                </div>
+                        <div className="space-y-2">
+                            <p className="grid-subtext text-center">Contact me</p>
+                            <div className="copy-container" onClick={handleCopy}>
+                                <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy" />
+                                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">gade.tintran76@gmail.com</p>
                             </div>
                         </div>
                     </div>
-
                 </div>
             </div>
         </section>
-    )
-}
-export default About
+    );
+};
+
+export default About;
