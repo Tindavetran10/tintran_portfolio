17291c86676b2e1209274c167eadcfb753adfb60
Tindavetran10 <102245594+Tindavetran10@users.noreply.github.com>
2025-04-30 18:39:49 +0700

    Finish final section

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import About from "./sections/About.jsx";
 import Projects from "./sections/Projects.jsx";
 import Contact from "./sections/Contact.jsx";
 import Footer from "./sections/Footer.jsx";
-import Experience from "./sections/Experience.jsx";
+import WorkExperience from "./sections/Experience.jsx";
 
 const App = () => {
     return (
@@ -14,7 +14,7 @@ const App = () => {
             <Hero />
             <About />
             <Projects />
-            <Experience />
+            <WorkExperience />
             <Contact />
             <Footer />
         </main>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -65,7 +65,7 @@ const Hero = () => {
     const sizes = calculateSizes(isSmall, isMobile, isTablet);
 
     return (
-        <section className="min-h-screen w-full flex-col relative">
+        <section className="min-h-screen w-full flex-col relative" id="home">
             <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
                 <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
                     Hi, I am Tin <span className="waving-hand"><PiHandWavingFill /></span>
